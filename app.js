c7fa2afe4004213b6611034b4303439b7048029a
kunal-jariya <kunal.jariya39@gmail.com>
2023-12-08 16:52:46 +0530

    build folder

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express()
 const cors = require("cors")
 const AllRoutes = require("./config/AllRoutes")
 
+// for Live Server 7,8,9
+const root = require("path").join(__dirname, "build")
+app.use(express.static(__dirname+"/assets"))
+app.use(express.static(root))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 app.use(AllRoutes)
+
+app.get("*", async(req, res)=>{
+    res.sendFile("index.html", {root})
+})
 
 const port = process.env.PORT || 8080
 app.listen(port, ()=>{
